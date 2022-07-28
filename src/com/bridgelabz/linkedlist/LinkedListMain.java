27f85f3cb79392685610db7db926816fb78dc89f
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-28 11:28:36 +0530

    Added Insert After method

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -23,6 +23,14 @@ public class LinkedListMain {
 		myObj2.append(70);
 		myObj2.display();
 		
+		System.out.println("Using InsertAfter method :  ");
+		LinkedListService<Integer> myObj3 = new LinkedListService<Integer>();
+		myObj3.append(56);
+		myObj3.append(70);
+		myObj3.insertAfter(myObj3.head ,30 );
+		myObj3.display();
+		
+		
 		
 		
 	}
--- src/com/bridgelabz/linkedlist/LinkedListService.java
+++ src/com/bridgelabz/linkedlist/LinkedListService.java
@@ -1,40 +1,44 @@
 package com.bridgelabz.linkedlist;
 
 public class LinkedListService<T> {
-	
-	private INode<T> head;
-	
-	public void add (T data) {
-		
+
+	 INode<T> head;
+
+	public void add(T data) {
+
 		INode<T> nNode = new INode<T>(data);
 		nNode.data = data;
-		nNode.next = head;	
+		nNode.next = head;
 		head = nNode;
 	}
+
 	public void display() {
 		INode<T> tempNode = head;
-		
+
 		while (tempNode != null) {
-			System.out.print(tempNode.data +" ");
+			System.out.print(tempNode.data + " ");
 			tempNode = tempNode.next;
 		}
 	}
-	
+
 	public void append(T data) {
 		INode<T> nNode = new INode<T>(data);
-		
-		if (head == null)
-		{
+
+		if (head == null) {
 			head = nNode;
-		}
-		else
-		{
+		} else {
 			INode<T> tempNode = head;
-			while(tempNode.next != null)
-			{
+			while (tempNode.next != null) {
 				tempNode = tempNode.next;
 			}
 			tempNode.next = nNode;
 		}
+	}
+
+	public void insertAfter(INode<T> prevNode, T data) {
+		INode<T> nNode = new INode<T>(data);
+		nNode.data = data;
+		nNode.next = prevNode.next;
+		prevNode.next = nNode;
 	}
 }
