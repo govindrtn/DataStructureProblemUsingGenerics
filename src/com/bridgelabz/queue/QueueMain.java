ead239836c31b6758bde1dd0c04cf06066dd30a2
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-31 11:14:34 +0530

    Added : enQueue method internally call linkedList Append method

---
+++ src/com/bridgelabz/queue/QueueMain.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.queue;
+
+public class QueueMain {
+
+	public static void main(String[] args) {
+
+		QueueServices<Integer> myQueue = new QueueServices<Integer>();
+		myQueue.enQueue(56);
+		myQueue.enQueue(30);
+		myQueue.enQueue(70);
+		myQueue.print();
+
+	}
+
+}
---
+++ src/com/bridgelabz/queue/QueueServices.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.queue;
+
+import com.bridgelabz.linkedlist.LinkedListService;
+
+public class QueueServices<T extends Comparable<T>> {
+
+	LinkedListService<T> linkedList = new LinkedListService<T>();
+
+	public void enQueue(T data) {
+
+		linkedList.append(data);
+	}
+	public void print() {
+		linkedList.display();
+	}
+}
